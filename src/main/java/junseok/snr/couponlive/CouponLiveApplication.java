8b2c8a74a2b204bc4e8cba10811514a4f22fa82f
JunseokOh <ixtears23@icloud.com>
2024-04-27 01:31:32 +0900

    Add 프로젝트 생성

---
+++ src/main/java/junseok/snr/couponlive/CouponLiveApplication.java
@@ -0,0 +1,13 @@
+package junseok.snr.couponlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/junseok/snr/couponlive/CouponLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.snr.couponlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
