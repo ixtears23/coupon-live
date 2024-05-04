2f13c2f2f8d8ae447c506f4e2474ab0c169d2f46
JunseokOh <ixtears23@icloud.com>
2024-05-04 22:50:21 +0900

    Test Coupon API 성공/실패 테스트 코드 추가

--- src/test/java/junseok/snr/couponlive/intrastructure/web/CouponControllerTest.java
+++ src/test/java/junseok/snr/couponlive/intrastructure/web/CouponControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -35,13 +37,53 @@ class CouponControllerTest {
         String jsonRequest = """
                                 {
                                   "userId": 1,
-                                  "couponId": 123
+                                  "couponId": 1
                                 }
                             """;
 
         mockMvc.perform(post("/v1/coupon")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonRequest))
+                .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+
+    @Test
+    void testIssueCouponFail_1() throws Exception {
+        String jsonRequest = """
+                                {
+                                  "userId": 1
+                                }
+                            """;
+
+        mockMvc.perform(post("/v1/coupon")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("couponId"))
+                .andExpect(jsonPath("$.message").value("must be greater than or equal to 1"));
+
+    }
+
+
+    @Test
+    void testIssueCouponFail_2() throws Exception {
+        String jsonRequest = "{}";
+
+        mockMvc.perform(post("/v1/coupon")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].code").value("userId"))
+                .andExpect(jsonPath("$[0].message").value("must be greater than or equal to 1"))
+                .andExpect(jsonPath("$[1].code").value("couponId"))
+                .andExpect(jsonPath("$[1].message").value("must be greater than or equal to 1"));
+
     }
 }
