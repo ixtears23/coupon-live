2e584bb2a8a8a29d538d76d8c6dca01193a737f5
JunseokOh <ixtears23@icloud.com>
2024-05-19 00:12:06 +0900

    Add 쿠폰 발급 예외 공통 처리 추가

--- src/main/java/junseok/snr/couponlive/intrastructure/web/coupon/CouponExceptionHandler.java
+++ src/main/java/junseok/snr/couponlive/intrastructure/web/coupon/CouponExceptionHandler.java
@@ -1,6 +1,9 @@
 package junseok.snr.couponlive.intrastructure.web.coupon;
 
+import junseok.snr.couponlive.domain.coupon.exception.CouponIssuanceException;
+import junseok.snr.couponlive.domain.coupon.exception.ErrorCode;
 import junseok.snr.couponlive.intrastructure.web.common.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice(basePackageClasses = CouponController.class)
 public class CouponExceptionHandler {
 
@@ -29,6 +33,17 @@ public class CouponExceptionHandler {
         }
 
         return new ResponseEntity<>(errorResponseList, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(CouponIssuanceException.class)
+    public ResponseEntity<ErrorResponse> handleCouponIssuanceException(CouponIssuanceException exception) {
+        log.error("=== 쿠폰 발급 에러", exception);
+        final ErrorCode errorCode = exception.getErrorCode();
+        return new ResponseEntity<>(
+                new ErrorResponse(errorCode.name(), errorCode.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
 
     }
 }
