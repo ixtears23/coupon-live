4968d2cb4de511c37b881f0cc0a3c133540361e0
JunseokOh <ixtears23@icloud.com>
2024-05-20 00:32:24 +0900

    Chore locust 파일 추가 및 worker scale 3 고정

--- locust/coupon-issue-task3.py
+++ locust/coupon-issue-task3.py
@@ -18,3 +18,12 @@ class CouponApi(FastHttpUser):
         }
         self.client.post("/v1/coupons/issue", json=data, name="쿠폰 발급 API")
 
+    @task(3)  # 이 태스크의 실행 비율을 기본값의 3배로 설정
+    def issue_coupon_2(self):
+        user_id = random.randint(1, 1000)  # 1과 1000 사이의 랜덤한 userId
+        data = {
+            "userId": user_id,
+            "couponId": 1,
+        }
+
+        self.client.post("/v1/coupon", json=data, name="쿠폰 발급 API(x3)")
